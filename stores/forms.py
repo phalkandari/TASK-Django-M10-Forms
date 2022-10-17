ed4f0544acd92599144dd5cc6e7ff2dbd3a970eb
phalkandari <mohammad.kandary@gmail.com>
2022-10-17 12:11:54 +0300

    Finished 1st part (Create Form)

---
+++ stores/forms.py
@@ -0,0 +1,8 @@
+from dataclasses import fields
+from django import forms
+from .models import StoreItem
+
+class StoreItemForm(forms.ModelForm):
+    class Meta:
+        model = StoreItem
+        fields = ['name', 'description', 'kind', 'price']
--- stores/views.py
+++ stores/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import StoreItemForm
 
 from stores import models
 
@@ -10,3 +11,14 @@ def get_store_items(request: HttpRequest) -> HttpResponse:
         "store_items": store_items,
     }
     return render(request, "store_item_list.html", context)
+
+def create_store_item(request):
+    form = StoreItemForm() #Instantiation of an instance
+    if request.method == "POST":
+        form = StoreItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("store-item-list")
+    
+    context = {'form': form}
+    return render(request, 'create_store_item.html', context)
